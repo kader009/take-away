c3c667bbbf8dbec18f5beb1a5ab00d7dc2861eec
kader009 <kadermollarasel000@gmail.com>
2024-11-19 22:40:19 +0600

    pass by reference

--- src/script.js
+++ src/script.js
@@ -1,6 +1,26 @@
 var numbers = 10;
 var numbers = 12;
 
-console.log(numbers);
+// console.log(numbers);
+
+let a = 6;
+
+function modifyValue(b) {
+  b = 5;
+}
+
+modifyValue(a);
+// console.log(a);
+
+// pass by reference
+
+let obj = { value: 5 };
+
+function modifyReference(y){
+  y.value = 15
+}
+
+modifyReference(obj)
+console.log(obj.value);
 
 
