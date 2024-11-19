5b7c59ef1bf05be383036fe132fb8b5907450b93
kader009 <kadermollarasel000@gmail.com>
2024-11-19 22:24:39 +0600

    let instence

--- src/script.js
+++ src/script.js
@@ -1,23 +1,6 @@
-const text = document.getElementById('text');
-const submit = document.getElementById('submit');
-const show = document.getElementById('show');
+var numbers = 10;
+var numbers = 12;
 
-submit.addEventListener('click', () => {
-  const inputValue = text.value.trim();
+console.log(numbers);
 
-  const listItem = document.createElement('li');
-  const taskItem = document.createElement('span');
-  const deletebtn = document.createElement('button');
-  deletebtn.innerText = 'Delete';
-  deletebtn.style.marginLeft = '10px';
-  taskItem.innerHTML = inputValue;
 
-  deletebtn.addEventListener('click', () => {
-    show.removeChild(listItem);
-  });
-
-  listItem.appendChild(taskItem);
-  listItem.appendChild(deletebtn);
-  show.appendChild(listItem);
-  text.value = '';
-});
