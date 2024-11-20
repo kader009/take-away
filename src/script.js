f18db0a1212841e79e84ca5747928e3459471ac7
kader009 <kadermollarasel000@gmail.com>
2024-11-20 23:35:14 +0600

    find prime number

--- src/script.js
+++ src/script.js
@@ -75,3 +75,12 @@ function factorial(number) {
   }
 }
 
+// Write a JavaScript function to check if a given number is prime.
+
+function isPrime(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
