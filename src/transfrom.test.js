787a9ee0a2cfc43a828c5916addef3d6ccecbcb0
kader009 <kadermollarasel000@gmail.com>
2024-11-26 19:37:15 +0600

    put all thing inside describe

--- src/transfrom.test.js
+++ src/transfrom.test.js
@@ -1,10 +1,13 @@
-import { it, expect } from 'vitest';
+import { it, expect, describe } from 'vitest';
 import { transfromNumber } from './transfromNum';
 
-it('this is test for string to number', () => {
-  const stringnumber = '9';
-  const response = transfromNumber(stringnumber);
+describe('discover', () => {
+  it('this is test for string to number', () => {
+    const stringnumber = '9';
+    const response = transfromNumber(stringnumber);
 
-  expect(response).toBeTypeOf('number');
-  expect(response).not.toBeTypeOf('string')
+    expect(response).toBeTypeOf('number');
+    expect(response).not.toBeTypeOf('string');
+    expect(isNaN(response)).not.toBe(true);
+  });
 });
