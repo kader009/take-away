711fe3ab75413ca65923ed0aa8d7b6cab3210c7e
Md Abdul Kader Molla <kadermollarasel000@gmail.com>
2025-10-07 19:35:54 +0600

    factory pattern added

--- src/pattern.js
+++ src/pattern.js
@@ -4,7 +4,7 @@ const factory = (function () {
   let privateVariable = `This is a private variable`;
 
   function privateFunction() {
-    console.log('This Is A Private Function');
+    // console.log('This Is A Private Function');
   }
 
   return {
@@ -14,4 +14,39 @@ const factory = (function () {
 })();
 
 factory.privateFunction();
-console.log(factory.privateVariable);
+// console.log(factory.privateVariable);
+
+// factory pattern are here
+function createProduct(name, price) {
+  let product = 30;
+
+  return {
+    name,
+    price,
+
+    stock() {
+      console.log(`we have ${product} in stock`);
+    },
+
+    buy(quantity) {
+      if (quantity <= product) {
+        product -= quantity;
+        console.log(
+          `you booked product ${quantity} - ${product} left in stock.`
+        );
+      } else {
+        console.log(`we only have ${product} in stock`);
+      }
+    },
+
+    refill(quantity) {
+      product += quantity;
+      console.log(
+        `you refilled ${quantity} products, now we have ${product} in stock.`
+      );
+    },
+  };
+}
+
+let book = createProduct("book", 200);
+book.buy(10);
