215c72acc449e1c5efc6b9a839efa65eb79bde47
Md Abdul Kader Molla <kadermollarasel000@gmail.com>
2025-10-07 19:14:12 +0600

    module pattern

--- src/pattern.js
+++ src/pattern.js
@@ -1,1 +1,17 @@
-console.log('object');
+// module pattern
+
+const factory = (function () {
+  let privateVariable = `This is a private variable`;
+
+  function privateFunction() {
+    console.log('This Is A Private Function');
+  }
+
+  return {
+    privateVariable,
+    privateFunction,
+  };
+})();
+
+factory.privateFunction();
+console.log(factory.privateVariable);
